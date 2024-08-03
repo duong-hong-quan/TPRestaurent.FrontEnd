a4a28963adfd59383d62643758314719f2badc73
QuanDH <hongquan.contact@gmail.com>
2024-08-03 07:53:19 +0700

    commit

---
+++ src/components/slider-home/SliderHome.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SliderHome = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: "linear",
+  };
+
+  const slides = [
+    {
+      id: 1,
+      image:
+        "https://s3-alpha-sig.figma.com/img/62f9/82bc/377a67314fcee620f0c8791bf2c0b7f2?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DfbUwQo1s9YMArTXLgR9LxPMiEAHWlOLc5KWg2Ktoqtvg8Q8LGJECW6lj~GSwNKHKRhDBjRfyTxcHeaBjKLIkomAY17MXuGlzH4nB1QO6YBlFTuNQDvEiqe1qtjBDY6HSkcmP2KkxiSYrGRq-LQoMIBt5T5i1IxyCQCgjeKGJHT~-MzNdB25H-LHwbxW4JcMRDzes4EGou0LeSN~fgz1oufcDXduzZg4dzbYDqyVH1ABCDdnDmucPgYrZCrXPun~ff7zfI3RtHtPG23VZXhMwXm~pZsJCgve1gqbMV3p5ZlIcWkS9c9P1JX0R~RMLt4FsVj0D66VEtQv3LFZoJ7~yg__",
+      alt: "Món ăn 1",
+      title: "Món Ăn Đặc Biệt 1",
+      description: "Thưởng thức hương vị độc đáo của món ăn đặc biệt này.",
+    },
+    {
+      id: 2,
+      image:
+        "https://s3-alpha-sig.figma.com/img/62f9/82bc/377a67314fcee620f0c8791bf2c0b7f2?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DfbUwQo1s9YMArTXLgR9LxPMiEAHWlOLc5KWg2Ktoqtvg8Q8LGJECW6lj~GSwNKHKRhDBjRfyTxcHeaBjKLIkomAY17MXuGlzH4nB1QO6YBlFTuNQDvEiqe1qtjBDY6HSkcmP2KkxiSYrGRq-LQoMIBt5T5i1IxyCQCgjeKGJHT~-MzNdB25H-LHwbxW4JcMRDzes4EGou0LeSN~fgz1oufcDXduzZg4dzbYDqyVH1ABCDdnDmucPgYrZCrXPun~ff7zfI3RtHtPG23VZXhMwXm~pZsJCgve1gqbMV3p5ZlIcWkS9c9P1JX0R~RMLt4FsVj0D66VEtQv3LFZoJ7~yg__",
+      alt: "Món ăn 2",
+      title: "Món Ăn Đặc Biệt 2",
+      description: "Khám phá hương vị mới mẻ với món ăn đặc sắc này.",
+    },
+    {
+      id: 3,
+      image:
+        "https://s3-alpha-sig.figma.com/img/62f9/82bc/377a67314fcee620f0c8791bf2c0b7f2?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DfbUwQo1s9YMArTXLgR9LxPMiEAHWlOLc5KWg2Ktoqtvg8Q8LGJECW6lj~GSwNKHKRhDBjRfyTxcHeaBjKLIkomAY17MXuGlzH4nB1QO6YBlFTuNQDvEiqe1qtjBDY6HSkcmP2KkxiSYrGRq-LQoMIBt5T5i1IxyCQCgjeKGJHT~-MzNdB25H-LHwbxW4JcMRDzes4EGou0LeSN~fgz1oufcDXduzZg4dzbYDqyVH1ABCDdnDmucPgYrZCrXPun~ff7zfI3RtHtPG23VZXhMwXm~pZsJCgve1gqbMV3p5ZlIcWkS9c9P1JX0R~RMLt4FsVj0D66VEtQv3LFZoJ7~yg__",
+      alt: "Món ăn 3",
+      title: "Món Ăn Đặc Biệt 3",
+      description: "Trải nghiệm ẩm thực tuyệt vời với món ăn này.",
+    },
+  ];
+
+  return (
+    <div className="relative h-screen">
+      <Slider {...settings} className="h-full">
+        {slides.map((slide) => (
+          <div key={slide.id} className="relative h-screen">
+            <img
+              src={slide.image}
+              alt={slide.alt}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-white px-4">
+              <h2 className="text-4xl md:text-6xl font-bold mb-4 text-center">
+                {slide.title}
+              </h2>
+              <p className="text-xl md:text-2xl text-center max-w-2xl">
+                {slide.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default SliderHome;
--- src/pages/common/home-page/HomePage.jsx
+++ src/pages/common/home-page/HomePage.jsx
@@ -3,17 +3,14 @@ import IntroHome from "../../../components/intro-home/IntroHome";
 import DishCard from "../../../components/menu-dish/dish-card/DishCard";
 import MenuDish from "../../../components/menu-dish/MenuDish";
 import Reservation from "../../../components/reservation/Reservation";
+import SliderHome from "../../../components/slider-home/SliderHome";
 import TopFeedback from "../../../components/top-feedback/TopFeedback";
 import TopVoucher from "../../../components/top-voucher/TopVoucher";
 
 export const HomePage = () => {
   return (
     <div className="container mx-auto">
-      <img
-        src="https://s3-alpha-sig.figma.com/img/62f9/82bc/377a67314fcee620f0c8791bf2c0b7f2?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DfbUwQo1s9YMArTXLgR9LxPMiEAHWlOLc5KWg2Ktoqtvg8Q8LGJECW6lj~GSwNKHKRhDBjRfyTxcHeaBjKLIkomAY17MXuGlzH4nB1QO6YBlFTuNQDvEiqe1qtjBDY6HSkcmP2KkxiSYrGRq-LQoMIBt5T5i1IxyCQCgjeKGJHT~-MzNdB25H-LHwbxW4JcMRDzes4EGou0LeSN~fgz1oufcDXduzZg4dzbYDqyVH1ABCDdnDmucPgYrZCrXPun~ff7zfI3RtHtPG23VZXhMwXm~pZsJCgve1gqbMV3p5ZlIcWkS9c9P1JX0R~RMLt4FsVj0D66VEtQv3LFZoJ7~yg__"
-        alt=""
-        className="w-full my-10"
-      />
+      <SliderHome />
       <TopVoucher />
       <IntroHome />
       <BestSeller />
