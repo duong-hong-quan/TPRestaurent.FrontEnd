3f47899f6e1b5f80c3eedff341a62e8aa40fee02
QuanDH <hongquan.contact@gmail.com>
2024-08-08 15:11:51 +0700

    commit menu page

--- src/pages/common/menu-page/MenuPage.jsx
+++ src/pages/common/menu-page/MenuPage.jsx
@@ -1,6 +1,41 @@
-import DishCard from "../../../components/menu-dish/dish-card/DishCard";
+import { useEffect, useState } from "react";
+import { Button } from "@material-tailwind/react";
+import ComboCard from "../../../components/menu-dish/ComboCard";
+import { Pagination } from "../../../components/pagination/Pagination";
+import { getAllCombo } from "../../../api/comboApi";
+import LoadingOverlay from "../../../components/loading/LoadingOverlay";
+import { toast } from "react-toastify";
 
 const MenuPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [combos, setCombos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(10); // Example total number of pages
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const fetchCombos = async () => {
+    try {
+      setIsLoading(true);
+      const data = await getAllCombo(searchQuery, currentPage, totalPages);
+      if (data?.isSuccess) {
+        setCombos(data?.result?.items);
+        setTotalPages(data?.result?.totalPages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCombos();
+  }, [searchQuery, currentPage]);
+
   const categories = [
     "Món khai vị",
     "Món chính",
@@ -8,30 +43,53 @@ const MenuPage = () => {
     "Đồ uống",
     "Đặc sản",
   ];
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div className="my-16">
-      <h1 className="uppercase font-bold text-center">
+      <LoadingOverlay isLoading={isLoading} />
+      <h1 className="uppercase font-bold text-center text-3xl mb-8">
         Khám phá thực đơn của chúng tôi
       </h1>
 
-      <div className="flex flex-wrap justify-center gap-4 mt-10">
+      <div className="flex flex-wrap justify-center gap-4 mb-10">
         {categories.map((category, index) => (
-          <button
+          <Button
             key={index}
-            className="px-4 py-2 border-2 border-red-500 rounded-md hover:bg-red-100 transition-colors duration-300"
+            variant="outlined"
+            color="red"
+            className="rounded-md transition-colors duration-300"
           >
             {category}
-          </button>
+          </Button>
         ))}
       </div>
 
-      <div className="container grid grid-cols-4">
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
+      <div className="flex justify-center mb-10">
+        <input
+          type="text"
+          placeholder="Tìm kiếm món ăn..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="w-full max-w-md border border-gray-500 rounded-md p-2"
+        />
       </div>
+
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {combos.map((combo) => (
+          <ComboCard key={combo.comboId} combo={combo} />
+        ))}
+      </div>
+      <Pagination
+        page={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
+
 export default MenuPage;
