a436ab59df9bc9f885ccd6f8a116bca26cb9ea4a
bichphuong1304 <bichphuong13042002@gmail.com>
2024-09-26 22:13:27 +0700

    fix(List Order): Fix UI

--- src/pages/kitchen/order-management/OrderManagement.jsx
+++ src/pages/kitchen/order-management/OrderManagement.jsx
@@ -24,11 +24,11 @@ const menuItems = [
 
 const TabStatusKitchen = ({ selected }) => {
   return (
-    <div className="flex justify-start">
+    <div className="flex justify-start mb-4">
       {menuItems.map((item) => (
         <Button
           key={item.key}
-          className={`bg-white text-black px-4 py-2 rounded-tl-md rounded-tr-md rounded-bl-none rounded-br-none mr-1 min-h-10 min-w-56 shadow-md ${
+          className={`bg-white text-gray-800 px-4 py-2 rounded-tl-md rounded-tr-md  font-semibold text-base rounded-bl-none rounded-br-none mr-1 min-h-10 min-w-56 shadow-md ${
             selected === item.key ? "text-red-900" : ""
           }`}
         >
@@ -43,13 +43,20 @@ const OrderTag = ({ status, index }) => {
   const [textColor, setTextColor] = useState("");
 
   const baseClass =
-    "flex items-center rounded-md border text-sm font-bold shadow-md w-fit px-8 py-2 transition-all duration-300 ease-in-out ";
+    "flex items-center rounded-md border-2 text-sm font-bold shadow-md w-fit px-8 py-2 transition-all duration-300 ease-in-out ";
 
   const statusColors = {
     waiting: "#C01D2E",
     preparing: "#E3B054",
     done: "#468764",
-    cancelled: "#E3B054",
+    cancelled: "#545454",
+  };
+
+  const statusBgColors = {
+    waiting: "#FFE9EC",
+    preparing: "#FFFCD4",
+    done: "#D0FFDD",
+    cancelled: "#DBDBDB",
   };
 
   const statusIcons = {
@@ -65,7 +72,7 @@ const OrderTag = ({ status, index }) => {
   }, [status]);
 
   const tagStyle = {
-    backgroundColor: `${statusColors[status]}50`, // 80 is 50% opacity in hex
+    backgroundColor: `${statusBgColors[status]}`, // 80 is 50% opacity in hex
     borderColor: statusColors[status],
     color: textColor,
   };
@@ -83,6 +90,7 @@ export const orderData = {
   tableNumber: 1,
   items: [
     {
+      id: 1,
       name: "Vegetable Mixups",
       description: "Vegetable Fritters with Egg",
       price: 5.3,
@@ -91,6 +99,16 @@ export const orderData = {
         "https://s3-alpha-sig.figma.com/img/cede/f2fa/0a35c8a1d8f399032cceab7502d20d6e?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EQ99YZl85afRTgMJdgfHOdP6fZD0F5qjs0kA-X51P-rr5dmTi1jRF1vtoobwu1JKOKR5VMeV2HTbA3pTo9GooTtuP0BbK1ytNde0UMQWzQa6nwToyHJ-X3LPKCmodj4391A0-t70OivGlSsKu6z~LuzLlXcwWIt~XcbxG5IyGiFjXWYpiMfG5j1tL9ObE1oT9dza9epR484hU1Ipm~f9F43neg7QkGUFHzj~0SYhVigY6Pet4DP-qOaSAKJVJnDaa11j2ZtsgBnMsYTXAUSxl-a3MZQuYIcFP8QtfuQYpb2HUvag9gimV7PhxT-KM65mXnkV75PscfY-URT1wTHPCQ__",
     },
     {
+      id: 2,
+      name: "Chinese Takeout Disj",
+      description: "Fresh Prawn mix salad",
+      price: 5.3,
+      quantity: 1,
+      image:
+        "https://s3-alpha-sig.figma.com/img/cede/f2fa/0a35c8a1d8f399032cceab7502d20d6e?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EQ99YZl85afRTgMJdgfHOdP6fZD0F5qjs0kA-X51P-rr5dmTi1jRF1vtoobwu1JKOKR5VMeV2HTbA3pTo9GooTtuP0BbK1ytNde0UMQWzQa6nwToyHJ-X3LPKCmodj4391A0-t70OivGlSsKu6z~LuzLlXcwWIt~XcbxG5IyGiFjXWYpiMfG5j1tL9ObE1oT9dza9epR484hU1Ipm~f9F43neg7QkGUFHzj~0SYhVigY6Pet4DP-qOaSAKJVJnDaa11j2ZtsgBnMsYTXAUSxl-a3MZQuYIcFP8QtfuQYpb2HUvag9gimV7PhxT-KM65mXnkV75PscfY-URT1wTHPCQ__",
+    },
+    {
+      id: 3,
       name: "Chinese Takeout Disj",
       description: "Fresh Prawn mix salad",
       price: 5.3,
@@ -102,7 +120,7 @@ export const orderData = {
 };
 const OrderCard = ({ orderNumber, date, items, tableNumber }) => {
   return (
-    <Card className="w-80 bg-pink-50 border border-[#C01D2E] mx-2 rounded-xl overflow-hidden">
+    <Card className="w-80 bg-white border-2 border-[#C01D2E] mx-2 my-2 rounded-xl overflow-hidden shadow-md">
       <div className="w-full">
         <div className="flex justify-between items-center">
           <div>
@@ -117,8 +135,8 @@ const OrderCard = ({ orderNumber, date, items, tableNumber }) => {
         </div>
       </div>
       <div className="pt-4">
-        {items.map((item, index) => (
-          <div key={index} className="flex items-center mb-4">
+        {items.slice(0, 2).map((item, index) => (
+          <div key={item.id} className="flex items-center mb-4">
             <img
               src={item.image}
               alt={item.name}
@@ -135,9 +153,12 @@ const OrderCard = ({ orderNumber, date, items, tableNumber }) => {
           </div>
         ))}
         {items.length > 2 && (
-          <p className="text-gray-500 mt-2">+ {items.length - 2} món khác</p>
+          <p className="text-gray-600 mt-2 font-bold">
+            + {items.length - 2} món khác
+          </p>
         )}
       </div>
+
       <div className="flex justify-between p-2">
         <Button className="bg-pink-50  border-none">
           <Eye size={24} className="h-6 w-6 text-red-800" />
@@ -174,7 +195,7 @@ const OrderManagement = () => {
         </Typography>
         <Input className="p-2 max-w-56" placeholder="Tìm đơn đặt món" />
       </div>
-      <div className="mt-10">
+      <div className="mt-5">
         <TabStatusKitchen selected={"all"} />
         <div className="flex gap-1 my-2">
           {fakeData.map((order, idx) => (
