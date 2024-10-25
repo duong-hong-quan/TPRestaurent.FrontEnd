72ed947082c077716b2a417b57096f90362dbca4
QuanDH <hongquan.contact@gmail.com>
2024-10-25 13:37:18 +0700

    enhance ux in navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
 import { Button, IconButton } from "@material-tailwind/react";
@@ -9,6 +9,9 @@ import { FaUser } from "react-icons/fa";
 import NotificationDemo from "../notification/NotificationDemo";
 import { logout } from "../../redux/features/authSlice";
 import { Search } from "lucide-react";
+import useCallApi from "../../api/useCallApi";
+import { NotificationApi } from "../../api/endpoint";
+
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isNotificationOpen, setIsNotificationOpen] = useState(false);
@@ -22,12 +25,40 @@ export const Navbar = () => {
   const user = useSelector((state) => state.user.user || {});
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { callApi, error } = useCallApi();
+  const [notifications, setNotifications] = useState([]);
+
+  const menuRef = useRef(null);
+  const notificationRef = useRef(null);
+  const searchRef = useRef(null);
+  const userMenuRef = useRef(null);
+
+  const fetchNotifications = async () => {
+    const response = await callApi(
+      `${NotificationApi.GET_ALL_NOTIFICATION_BY_USER}/${user.id}`,
+      "GET"
+    );
+    if (response.isSuccess) {
+      setNotifications(response.result?.items);
+    } else {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
   const toggleNotification = () => {
     setIsNotificationOpen(!isNotificationOpen);
+    setIsMenuUserOpen(false);
+    setIsOpen(false);
   };
 
   const toggleMenuUser = () => {
     setIsMenuUserOpen(!isMenuUserOpen);
+    setIsNotificationOpen(false);
+    setIsOpen(false);
   };
 
   const handleLogout = () => {
@@ -70,7 +101,37 @@ export const Navbar = () => {
     navigate(`/search?q=${encodeURIComponent(searchTerm.trim())}`);
   };
 
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    setIsNotificationOpen(false);
+    setIsMenuUserOpen(false);
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      notificationRef.current &&
+      !notificationRef.current.contains(event.target) &&
+      searchRef.current &&
+      !searchRef.current.contains(event.target) &&
+      userMenuRef.current &&
+      !userMenuRef.current.contains(event.target)
+    ) {
+      setIsMenuOpen(false);
+      setIsNotificationOpen(false);
+      setIsOpen(false);
+      setIsMenuUserOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="bg-[#9A0E1D] text-white shadow-lg sticky top-0 z-50">
@@ -127,29 +188,33 @@ export const Navbar = () => {
                   </Badge>
                 </NavLink>
               ))}
-            <div className="relative">
+            <div className="relative" ref={notificationRef}>
               {icons
                 .filter((i) => i.name === "Thông báo")
                 .map((item, index) => (
-                  <IconButton
-                    key={index}
-                    className="bg-transparent rounded-full shadow-none mx-0 px-0"
-                    onClick={toggleNotification}
-                  >
-                    <i
-                      className={`fa-solid ${item.icon} text-xl text-white`}
-                    ></i>
-                  </IconButton>
+                  <Badge count={notifications.length} key={index}>
+                    <IconButton
+                      className="bg-transparent rounded-full shadow-none mx-0 px-0"
+                      onClick={toggleNotification}
+                    >
+                      <i
+                        className={`fa-solid ${item.icon} text-xl text-white`}
+                      ></i>
+                    </IconButton>
+                  </Badge>
                 ))}
               {isNotificationOpen && (
-                <div className="absolute right-0 mt-2 w-[550px] bg-white rounded-md shadow-lg py-2 z-20">
-                  <NotificationDemo />
+                <div className="absolute right-0 mt-2 w-80 bg-white rounded-md shadow-lg py-2 z-20">
+                  <NotificationDemo notifications={notifications} />
                 </div>
               )}
             </div>
 
             {isOpen && (
-              <div className="left-1/2 -translate-x-1/2 top-20 absolute w-full px-4 sm:px-6 lg:px-8 z-50">
+              <div
+                className="left-1/2 -translate-x-1/2 top-20 absolute w-full px-4 sm:px-6 lg:px-8 z-50"
+                ref={searchRef}
+              >
                 <div className="max-w-7xl mx-auto">
                   <form className="flex flex-col sm:flex-row items-center gap-2 bg-white rounded-lg shadow-2xl p-2 mt-10">
                     <input
@@ -172,9 +237,13 @@ export const Navbar = () => {
             <IconButton
               variant="text"
               className="rounded-full text-white hover:bg-white/20 transition-all"
-              onClick={() => setIsOpen(!isOpen)}
+              onClick={() => {
+                setIsOpen(!isOpen);
+                setIsNotificationOpen(false);
+                setIsMenuUserOpen(false);
+              }}
             >
-             <Search />
+              <Search />
             </IconButton>
             {isEmptyObject(user) ? (
               <NavLink
@@ -184,52 +253,50 @@ export const Navbar = () => {
                 Đăng nhập
               </NavLink>
             ) : (
-              <>
-                <div className="relative">
-                  <button onClick={toggleMenuUser}>
-                    <FaUser className="inline-block mr-2" />
-                  </button>
-                  {isMenuUserOpen && (
-                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20">
+              <div className="relative" ref={userMenuRef}>
+                <button onClick={toggleMenuUser}>
+                  <FaUser className="inline-block mr-2" />
+                </button>
+                {isMenuUserOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20">
+                    <NavLink
+                      to="/user/"
+                      className="block px-4 py-2 text-center text-gray-800 hover:bg-gray-200"
+                    >
+                      Hồ sơ của tôi
+                    </NavLink>
+                    {user.mainRole === "ADMIN" && (
                       <NavLink
-                        to="/user/"
+                        to="/admin"
                         className="block px-4 py-2 text-center text-gray-800 hover:bg-gray-200"
                       >
-                        Hồ sơ của tôi
+                        Quản trị
                       </NavLink>
-                      {user.mainRole === "ADMIN" && (
-                        <NavLink
-                          to="/admin"
-                          className="block px-4 py-2 text-center text-gray-800 hover:bg-gray-200"
-                        >
-                          Quản trị
-                        </NavLink>
-                      )}
-                      {user.mainRole === "CHEF" && (
-                        <NavLink
-                          to="/kitchen"
-                          className="block px-4 py-2 text-center text-gray-800 hover:bg-gray-200"
-                        >
-                          Quản trị
-                        </NavLink>
-                      )}
-                      <button
-                        onClick={handleLogout}
-                        className="block w-full text-center px-4 py-2 text-gray-800 hover:bg-gray-200"
+                    )}
+                    {user.mainRole === "CHEF" && (
+                      <NavLink
+                        to="/kitchen"
+                        className="block px-4 py-2 text-center text-gray-800 hover:bg-gray-200"
                       >
-                        Đăng xuất
-                      </button>
-                    </div>
-                  )}
-                </div>
-              </>
+                        Quản trị
+                      </NavLink>
+                    )}
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full text-center px-4 py-2 text-gray-800 hover:bg-gray-200"
+                    >
+                      Đăng xuất
+                    </button>
+                  </div>
+                )}
+              </div>
             )}
           </div>
         </div>
 
         {/* Mobile menu */}
         {isMenuOpen && (
-          <div className="xl:hidden">
+          <div className="xl:hidden" ref={menuRef}>
             <ul className="pt-2 pb-4">
               {navItems.map((item) => (
                 <li key={item.name}>
