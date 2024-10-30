17726602dae15403891c9a72bc3d187890e8a518
QuanDH <108501806+duong-hong-quan@users.noreply.github.com>
2024-10-30 13:45:13 +0700

    Feat/dining area (#170)
    * add dining area
    
    * optimize loading
    
    * hot fix order detail
    
    * handle payment dine

--- src/components/order/modal/ModalOrderDetailAdmin.jsx
+++ src/components/order/modal/ModalOrderDetailAdmin.jsx
@@ -1,12 +1,14 @@
 import { Modal } from "antd";
 import OrderDetail from "../order-detail/OrderDetail";
 import OrderDetailAdmin from "../order-detail/OrderDetailAdmin";
+import useCallApi from "../../../api/useCallApi";
+import { OrderApi } from "../../../api/endpoint";
 
 const ModalOrderDetailAdmin = ({ reservation, visible, onClose }) => {
   return (
     <Modal open={visible} onCancel={onClose} footer={null} width={1400}>
       <div className="h-[650px] overflow-y-scroll">
-        <OrderDetailAdmin reservationData={reservation} />
+        <OrderDetailAdmin reservationData={reservation} onClose={onClose} />
       </div>
     </Modal>
   );
