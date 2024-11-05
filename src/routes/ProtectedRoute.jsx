755bf8f92ecae2e1be6431d83773d9ce6be60443
QuanDH <hongquan.contact@gmail.com>
2024-11-06 00:12:49 +0700

    handle logout realtime

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,15 +2,33 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { isEmptyObject } from "../util/Utility";
+import useCallApi from "../api/useCallApi";
+import { TokenApi } from "../api/endpoint";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/features/authSlice";
 
 export const ProtectedRoute = ({ children, role }) => {
   const user = useSelector((state) => state.user.user || {});
+  const { callApi, error, loading } = useCallApi();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const fetchCurrentToken = async () => {
+    debugger;
+    const currentTokenResponse = await callApi(
+      `${TokenApi.GET_USER_TOKEN_BY_IP}`,
+      "GET"
+    );
+    if (!currentTokenResponse.isSuccess) {
+      dispatch(logout());
+      navigate("/");
+    }
+  };
 
   useEffect(() => {
     if (isEmptyObject(user)) {
       navigate("/");
     } else {
+      fetchCurrentToken();
       switch (role) {
         case "admin":
           if (user.mainRole !== "ADMIN") {
