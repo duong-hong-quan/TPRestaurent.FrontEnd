1620f50da3eac47e82e166ee10afb19564c5f2d0
QuanDH <hongquan.contact@gmail.com>
2024-11-13 13:07:10 +0700

    add chatbot

--- README.md
+++ README.md
@@ -1,8 +1,86 @@
-# React + Vite
+# ROMS - Restaurant Order Management System
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Overview
 
-Currently, two official plugins are available:
+This project is a web application built with React, utilizing various libraries and tools such as Redux, Firebase, Tailwind CSS, and more. The application includes features like authentication, notifications, and a responsive design.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Folder Structure](#folder-structure)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/FA24-Capstone/TPRestaurent.FrontEnd
+   cd TPRestaurant.FrontEnd
+   ```
+
+2. **Install dependencies:**
+
+```bash
+
+npm install
+```
+
+3. **Create a .env file in the root directory and add your Firebase configuration:**
+
+```bash
+REACT_APP_FIREBASE_API_KEY=your-firebase-api-key
+REACT_APP_FIREBASE_AUTH_DOMAIN=your-firebase-auth-domain
+REACT_APP_FIREBASE_PROJECT_ID=your-firebase-project-id
+REACT_APP_FIREBASE_STORAGE_BUCKET=your-firebase-storage-bucket
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID=your-firebase-messaging-sender-id
+REACT_APP_FIREBASE_APP_ID=your-firebase-app-id
+```
+
+4. **Start the development server:**
+
+```bash
+npm run dev
+```
+
+5. **Features**
+   `Waiting for update...`
+
+6. **Folder structure**
+   restaurant-order-management-system/
+   ├── public/
+   ├── src/
+   │ ├── components/
+   │ ├── contexts/
+   │ ├── firebase.js
+   │ ├── hooks/
+   │ ├── pages/
+   │ ├── styles/
+   │ ├── App.js
+   │ ├── index.js
+   │ └── ...
+   ├── .env
+   ├── .gitignore
+   ├── package.json
+   ├── README.md
+   └── ...
+
+7. **Contribute**
+
+   1. Contributing
+   2. Fork the repository.
+   3. Create a new branch:
+
+      git checkout -b feature-branch
+      Make your changes.
+
+   4. Commit your changes:
+
+      git commit -m 'Add some feature'
+      Push to the branch:
+
+      git push origin feature-branch
+      Open a pull request.
--- src/components/chat/ChatButton.jsx
+++ src/components/chat/ChatButton.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
-import { MessageCircle, X, Send } from "lucide-react";
+import React, { useState, useEffect, useRef } from "react";
+import { Send } from "lucide-react";
 import AI from "../../assets/imgs/AI.png";
+import useCallApi from "../../api/useCallApi";
+
 // Fake chat data
 const initialMessages = [
-  { id: 1, text: "Hello! How can I help you today?", sender: "bot" },
-  { id: 2, text: "I have a question about your services.", sender: "user" },
   {
-    id: 3,
-    text: "Of course! I'd be happy to help. What would you like to know?",
+    id: 1,
+    text: "Chào bạn tôi là trợ lý ảo AI của nhà hàng Thiên Phú",
+    sender: "bot",
+  },
+  {
+    id: 2,
+    text: "Bạn cần tôi giúp đỡ gì không?",
     sender: "bot",
   },
 ];
@@ -16,43 +21,69 @@ const ChatButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [messages, setMessages] = useState(initialMessages);
   const [newMessage, setNewMessage] = useState("");
+  const chatContainerRef = useRef(null);
 
   const toggleChat = () => setIsOpen(!isOpen);
+  const { callApi, loading } = useCallApi();
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (newMessage.trim()) {
       setMessages([
         ...messages,
         { id: messages.length + 1, text: newMessage, sender: "user" },
       ]);
       setNewMessage("");
-      // Simulate bot response
-      setTimeout(() => {
+
+      try {
+        const response = await callApi(
+          `chatbot/test-chat?message=${newMessage}`,
+          "GET"
+        );
         setMessages((prev) => [
           ...prev,
           {
             id: prev.length + 1,
-            text: "Thank you for your message. How else can I assist you?",
+            text: response.result,
             sender: "bot",
           },
         ]);
-      }, 1000);
+      } catch (err) {
+        console.error(err);
+        setMessages((prev) => [
+          ...prev,
+          {
+            id: prev.length + 1,
+            text: "Failed to get response from AI. Please try again.",
+            sender: "bot",
+          },
+        ]);
+      }
     }
   };
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   return (
     <div className="fixed z-50 bottom-[0.5rem] right-[0.5rem]">
       {isOpen ? (
-        <div className="bg-white  rounded-lg shadow-lg w-80 h-96 flex flex-col">
+        <div className="bg-white rounded-lg shadow-lg w-[400px] h-[550px]  flex flex-col">
           <div
             onClick={toggleChat}
             className="bg-red-900 flex justify-start items-center text-white rounded-tr-lg rounded-tl-lg px-4 py-2 cursor-pointer"
           >
             <img src={AI} alt="" className="h-12 w-12" />
             <span className="">MR.CHEF AI</span>
-            <span className="bg-[#4F970F] animate-pulse  h-3 w-3 rounded-full inline-block ml-2"></span>{" "}
+            <span className="bg-[#4F970F] animate-pulse h-3 w-3 rounded-full inline-block ml-2"></span>{" "}
           </div>
-          <div className="flex-grow p-4 overflow-y-auto bg-[#FFF9EF]">
+          <div
+            ref={chatContainerRef}
+            className="flex-grow p-4 overflow-y-auto bg-[#FFF9EF]"
+          >
             {messages.map((message) => (
               <div
                 key={message.id}
@@ -69,6 +100,13 @@ const ChatButton = () => {
                 </span>
               </div>
             ))}
+            {loading && (
+              <div className="mb-2 text-left">
+                <span className="inline-block p-2 text-white rounded-lg bg-red-500 animate-pulse">
+                  Thinking...
+                </span>
+              </div>
+            )}
           </div>
           <div className="p-4 border-t flex">
             <input
@@ -77,11 +115,12 @@ const ChatButton = () => {
               onChange={(e) => setNewMessage(e.target.value)}
               placeholder="Type a message..."
               className="flex-grow p-2 border rounded-xl mx-2"
-              onKeyPress={(e) => e.key === "Enter" && handleSendMessage()}
+              onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
             />
             <button
               onClick={handleSendMessage}
               className="bg-red-900 text-white p-2 rounded-xl hover:bg-red-800"
+              disabled={loading}
             >
               <Send size={20} />
             </button>
@@ -92,9 +131,9 @@ const ChatButton = () => {
           onClick={toggleChat}
           className="bg-red-900 animate-bounce flex justify-between items-center text-white rounded-tr-lg rounded-tl-lg px-4 py-2 cursor-pointer"
         >
-          <img src={AI} alt="" className="h-12 w-12" />
+          <img src={AI} alt="" className="h-12 w-12 object-cover" />
           <span className="animate-pulse ">MR.CHEF AI</span>
-          <span className="bg-[#4F970F] animate-pulse  h-3 w-3 rounded-full inline-block ml-2"></span>{" "}
+          <span className="bg-[#4F970F] animate-pulse h-3 w-3 rounded-full inline-block ml-2"></span>{" "}
         </div>
       )}
     </div>
